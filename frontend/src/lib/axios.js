c9e943a1a8209724baab79735962df8c8df1de79
nishanafabin <nishanafabin@gmail.com>
2025-09-30 12:04:30 +0530

    Your commit message here

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:3002/api" : "/api",
+	baseURL: import.meta.mode === "development" ? "http://localhost:3002/api" : "https://mern-ecommerce-backend.onrender.com/api",
 	withCredentials: true, // send cookies to the server
 });
 
